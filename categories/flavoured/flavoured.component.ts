281f9db3dcfa81e09f4229948ca2036df2855c62
lenatr99 <lt89715@student.uni-lj.si>
2024-11-30 16:24:37 +0100

    Files fix

--- categories/flavoured/flavoured.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-flavoured',
-  imports: [],
-  templateUrl: './flavoured.component.html',
-  styleUrl: './flavoured.component.css',
-})
-export class FlavouredComponent {}
